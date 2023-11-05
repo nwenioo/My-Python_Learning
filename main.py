ab8a72b5377dd4fb03f3924493398652970cd4ce
nwenioo <nwenioo@example.com>
2023-11-05 20:09:16 +0630

    Lesson5

---
+++ main.py
@@ -0,0 +1,15 @@
+#to print to console
+# print('hello world');
+
+# input function()
+# name=input(' Name :')
+# print('hello ' +name)
+
+# age=input('tell me your age ')
+# print('your age is  '+age)
+
+# area calculator (pi r**2)
+PI=3.13
+r=int(input('radius :'))
+area=PI*r**2
+print(area)
